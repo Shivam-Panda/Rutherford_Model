86b1aba781029b992b6fee495a18a619bcdd8bed
Shivam-Panda <panda.shivam81@bloomfield.org>
2024-07-08 16:24:19 -0400

    Page Moving Template Setup

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from tkinter import *
 from tkinter import ttk
-from thomson import thomson
-from rutherford import ruther
-
-def thom_render():
-    thomson.mainloop()
-    root.destroy()
-
-def ruther_render():
-    ruther.mainloop()
-    root.destroy()
+from pages.page1 import render
 
 root = Tk()
+
+def open_render():
+    root.destroy()
+    render()
 
 home = ttk.Frame(root, padding=50)
 home.grid()
 ttk.Label(home, text="Home Page").grid(column=0, row=0)
-ttk.Button(home, text="Thomson", command=thom_render).grid(column=0, row=1)
-ttk.Button(home, text="Rutherford", command=ruther_render).grid(column=1, row=1)
+ttk.Button(home, text="Open Second", command=open_render).grid(column=0, row=1)
+ttk.Button(home, text="Home Quit", command=root.destroy).grid(column=1, row=1)
 
 root.mainloop()
