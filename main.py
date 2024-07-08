58c5fb9902537e0b96f93106ea3c2e9857c532ae
Shivam-Panda <panda.shivam81@bloomfield.org>
2024-07-08 17:23:14 -0400

    Fix

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from tkinter import *
 from tkinter import ttk
-from pages.page1 import render
 
 root = Tk()
 
-def open_render():
-    root.destroy()
-    render()
+# def open_render():
+#     root.destroy()
+#     render()
 
 home = ttk.Frame(root, padding=50)
 home.grid()
 ttk.Label(home, text="Home Page").grid(column=0, row=0)
-ttk.Button(home, text="Open Second", command=open_render).grid(column=0, row=1)
+# ttk.Button(home, text="Open Second", command=open_render).grid(column=0, row=1)
 ttk.Button(home, text="Home Quit", command=root.destroy).grid(column=1, row=1)
 
 root.mainloop()
